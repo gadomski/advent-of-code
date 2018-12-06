e4a8544453ccf747130b08bcd63d876e077292fe
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-06 06:29:56 -0700

    Whoo, this one seems tricky

--- src/06.rs
+++ src/06.rs
@@ -3,5 +3,22 @@ extern crate failure;
 use failure::Error;
 
 fn main() -> Result<(), Error> {
+    let input = include_str!("../input/06.txt");
+    println!("Part 1: {}", size_of_largest_area(input)?);
+    Ok(())
+}
+
+fn size_of_largest_area(input: &str) -> Result<u64, Error> {
     unimplemented!()
+}
+
+#[test]
+fn part_1() {
+    let input = "1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9";
+    assert_eq!(17, size_of_largest_area(input).unwrap());
 }
