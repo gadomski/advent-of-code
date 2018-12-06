170a7dd80ca8038662d6eb31b14f8c82788b9f31
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-06 06:37:56 -0700

    Parse points

--- src/06.rs
+++ src/06.rs
@@ -1,6 +1,10 @@
+#[macro_use]
 extern crate failure;
+extern crate regex;
 
 use failure::Error;
+use regex::Regex;
+use std::str::FromStr;
 
 fn main() -> Result<(), Error> {
     let input = include_str!("../input/06.txt");
@@ -9,7 +13,33 @@ fn main() -> Result<(), Error> {
 }
 
 fn size_of_largest_area(input: &str) -> Result<u64, Error> {
+    let coordinates = input
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<Vec<Point>, Error>>()?;
     unimplemented!()
+}
+
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Debug, Fail)]
+#[fail(display = "invalid point format: {}", _0)]
+struct InvalidPointFormat(String);
+
+impl FromStr for Point {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Point, Error> {
+        let regex = Regex::new(r"^(\d+), (\d+)$")?;
+        let captures = regex.captures(s).ok_or(InvalidPointFormat(s.to_string()))?;
+        Ok(Point {
+            x: captures[1].parse()?,
+            y: captures[2].parse()?,
+        })
+    }
 }
 
 #[test]
