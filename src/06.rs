2cf78087ad72d550b624ed01e3c8d79c22ff7605
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-06 07:12:37 -0700

    Cool

--- src/06.rs
+++ src/06.rs
@@ -4,6 +4,9 @@ extern crate regex;
 
 use failure::Error;
 use regex::Regex;
+use std::collections::{HashMap, HashSet};
+use std::i64;
+use std::iter::FromIterator;
 use std::str::FromStr;
 
 fn main() -> Result<(), Error> {
@@ -12,23 +15,101 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn size_of_largest_area(input: &str) -> Result<u64, Error> {
+fn size_of_largest_area(input: &str) -> Result<usize, Error> {
     let coordinates = input
         .lines()
         .map(|line| line.parse())
         .collect::<Result<Vec<Point>, Error>>()?;
-    unimplemented!()
+    let min = Point {
+        x: coordinates
+            .iter()
+            .map(|point| point.x)
+            .min()
+            .ok_or(NoPoints)?,
+        y: coordinates
+            .iter()
+            .map(|point| point.y)
+            .min()
+            .ok_or(NoPoints)?,
+    };
+    let max = Point {
+        x: coordinates
+            .iter()
+            .map(|point| point.x)
+            .max()
+            .ok_or(NoPoints)?,
+        y: coordinates
+            .iter()
+            .map(|point| point.y)
+            .max()
+            .ok_or(NoPoints)?,
+    };
+    let mut map = HashMap::new();
+    let mut candidate_coordinates: HashSet<Point> =
+        HashSet::from_iter(coordinates.iter().map(|&p| p));
+    for y in min.y..=max.y {
+        for x in min.x..=max.x {
+            let point = Point { x: x, y: y };
+            let closest_coordinate = closest_coordinate(&coordinates, point);
+            if let Some(closest_coordinate) = closest_coordinate {
+                if x == min.x || x == max.x || y == min.y || y == max.y {
+                    candidate_coordinates.remove(&closest_coordinate);
+                }
+            }
+            map.insert(point, closest_coordinate);
+        }
+    }
+    candidate_coordinates
+        .into_iter()
+        .map(|coordinate| {
+            map.values()
+                .filter(|closest_coordinate| {
+                    closest_coordinate
+                        .map(|closest_coordinate| closest_coordinate == coordinate)
+                        .unwrap_or(false)
+                }).count()
+        }).max()
+        .ok_or(NoFiniteAreas.into())
 }
 
-#[derive(Debug)]
+fn closest_coordinate(coordinates: &[Point], point: Point) -> Option<Point> {
+    let mut min_distance = i64::MAX;
+    let mut closest_coordinate = None;
+    for coordinate in coordinates {
+        let distance = coordinate.distance_to(point);
+        if distance == min_distance {
+            closest_coordinate = None;
+        } else if distance < min_distance {
+            min_distance = distance;
+            closest_coordinate = Some(*coordinate);
+        }
+    }
+    closest_coordinate
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Point {
-    x: u64,
-    y: u64,
+    x: i64,
+    y: i64,
 }
 
 #[derive(Debug, Fail)]
 #[fail(display = "invalid point format: {}", _0)]
 struct InvalidPointFormat(String);
+
+#[derive(Debug, Fail)]
+#[fail(display = "no points provided in input")]
+struct NoPoints;
+
+#[derive(Debug, Fail)]
+#[fail(display = "no finite areas found")]
+struct NoFiniteAreas;
+
+impl Point {
+    fn distance_to(&self, point: Point) -> i64 {
+        (self.x - point.x).abs() + (self.y - point.y).abs()
+    }
+}
 
 impl FromStr for Point {
     type Err = Error;
