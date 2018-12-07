7028b1f1ce6e6f9176003904475a3ca114a30a1a
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-07 07:27:27 -0700

    Did it

--- src/07.rs
+++ src/07.rs
@@ -1,6 +1,10 @@
+#[macro_use]
 extern crate failure;
+extern crate regex;
 
 use failure::Error;
+use regex::Regex;
+use std::collections::HashMap;
 
 fn main() -> Result<(), Error> {
     let input = include_str!("../input/07.txt");
@@ -9,8 +13,54 @@ fn main() -> Result<(), Error> {
 }
 
 fn correct_order(input: &str) -> Result<String, Error> {
-    unimplemented!()
+    let mut requirements = HashMap::new();
+    let regex = Regex::new(r"^Step (\w+) must be finished before step (\w+) can begin.$")?;
+    for line in input.lines() {
+        let captures = regex.captures(line).ok_or(InvalidLine(line.to_string()))?;
+        let parent = &captures[1];
+        let child = &captures[2];
+        requirements
+            .entry(parent.to_string())
+            .or_insert_with(Vec::new)
+            .push(child.to_string())
+    }
+    let first = requirements
+        .keys()
+        .filter(|step| {
+            !requirements
+                .values()
+                .any(|children| children.contains(step))
+        }).next()
+        .ok_or(Circular(requirements.clone()))?
+        .clone();
+
+    let mut available = vec![first];
+    let mut done = String::new();
+    while !available.is_empty() {
+        available.sort();
+        let step = available.remove(0);
+        done.push_str(&step);
+        if let Some(children) = requirements.remove(&step) {
+            for child in children {
+                if !requirements
+                    .values()
+                    .any(|children| children.contains(&child))
+                {
+                    available.push(child);
+                }
+            }
+        }
+    }
+    Ok(done)
 }
+
+#[derive(Debug, Fail)]
+#[fail(display = "invalid line: {}", _0)]
+struct InvalidLine(String);
+
+#[derive(Debug, Fail)]
+#[fail(display = "circular requirements: {:?}", _0)]
+struct Circular(HashMap<String, Vec<String>>);
 
 #[test]
 fn part_1() {
