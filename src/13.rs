8af12515bd278966d7329801e81e384884538717
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-15 17:13:12 -0700

    Whoa

--- src/13.rs
+++ src/13.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 
@@ -17,11 +18,28 @@ fn first_crash(input: &str) -> Result<String, Error> {
 struct Tracks {}
 
 #[derive(Debug)]
+struct Track {}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Location {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug)]
 enum Error {}
 
 impl FromStr for Tracks {
     type Err = Error;
     fn from_str(s: &str) -> Result<Tracks, Error> {
+        let mut tracks = HashMap::new();
+        for (row, line) in s.lines().enumerate() {
+            for (col, c) in s.chars().enumerate() {
+                if let Some(track) = Track::new(c) {
+                    tracks.insert(Location::new(col, row), track);
+                }
+            }
+        }
         Ok(Tracks {})
     }
 }
@@ -29,6 +47,18 @@ impl FromStr for Tracks {
 impl fmt::Display for Tracks {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "tracks!")
+    }
+}
+
+impl Track {
+    fn new(c: char) -> Option<Track> {
+        unimplemented!()
+    }
+}
+
+impl Location {
+    fn new(x: usize, y: usize) -> Location {
+        unimplemented!()
     }
 }
 
