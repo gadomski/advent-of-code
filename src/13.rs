4d709df317a7a56afdcc2b4bcd4bdbff91c7bb36
Pete Gadomski <pete.gadomski@gmail.com>
2018-12-15 17:07:17 -0700

    Nice

--- src/13.rs
+++ src/13.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::fmt;
 use std::str::FromStr;
 
 fn main() -> Result<(), Error> {
@@ -8,138 +8,27 @@ fn main() -> Result<(), Error> {
 }
 
 fn first_crash(input: &str) -> Result<String, Error> {
-    let mut map: Map = input.parse()?;
-    while !map.has_crash() {
-        map.tick()?;
-    }
+    let tracks: Tracks = input.parse()?;
+    println!("{}", tracks);
     unimplemented!()
 }
 
 #[derive(Debug)]
-struct Map {
-    track: HashMap<Location, Piece>,
-    carts: Vec<Cart>,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct Location {
-    y: usize,
-    x: usize,
-}
+struct Tracks {}
 
 #[derive(Debug)]
-enum Piece {
-    Vertical,
-    Horizontal,
-    RightCurve,
-    LeftCurve,
-    Intersection,
-}
+enum Error {}
 
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Cart {
-    location: Location,
-    next_turn_direction: Turn,
-    direction: Direction,
-    has_crashed: bool,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Turn {
-    Left,
-    Straight,
-    Right,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-#[derive(Debug)]
-enum Error {
-    CantAdvanceACrashedCart(Cart),
-    UnknownPiece(char, Location),
-}
-
-impl Map {
-    fn has_crash(&self) -> bool {
-        self.carts.iter().any(|cart| cart.has_crashed())
-    }
-
-    fn tick(&mut self) -> Result<(), Error> {
-        self.carts.sort();
-        for cart in &mut self.carts {
-            if cart.has_crashed {
-                return Err(Error::CantAdvanceACrashedCart(cart.clone()));
-            } else {
-                unimplemented!()
-            }
-        }
-        Ok(())
-    }
-}
-
-impl FromStr for Map {
+impl FromStr for Tracks {
     type Err = Error;
-    fn from_str(s: &str) -> Result<Map, Error> {
-        let mut track = HashMap::new();
-        let mut carts = Vec::new();
-        for (row, line) in s.lines().enumerate() {
-            for (col, c) in line.chars().enumerate() {
-                let location = Location { x: col, y: row };
-                let piece = match c {
-                    '|' => Some(Piece::Vertical),
-                    '-' => Some(Piece::Horizontal),
-                    '\\' => Some(Piece::LeftCurve),
-                    '/' => Some(Piece::RightCurve),
-                    '+' => Some(Piece::Intersection),
-                    ' ' => None,
-                    '>' => {
-                        carts.push(Cart::new(location, Direction::Right));
-                        Some(Piece::Horizontal)
-                    }
-                    'v' => {
-                        carts.push(Cart::new(location, Direction::Down));
-                        Some(Piece::Vertical)
-                    }
-                    '<' => {
-                        carts.push(Cart::new(location, Direction::Left));
-                        Some(Piece::Horizontal)
-                    }
-                    '^' => {
-                        carts.push(Cart::new(location, Direction::Up));
-                        Some(Piece::Vertical)
-                    }
-                    _ => return Err(Error::UnknownPiece(c, location)),
-                };
-                if let Some(piece) = piece {
-                    track.insert(location, piece);
-                }
-            }
-        }
-        Ok(Map {
-            track: track,
-            carts: carts,
-        })
+    fn from_str(s: &str) -> Result<Tracks, Error> {
+        Ok(Tracks {})
     }
 }
 
-impl Cart {
-    fn new(location: Location, direction: Direction) -> Cart {
-        Cart {
-            location: location,
-            direction: direction,
-            next_turn_direction: Turn::Left,
-            has_crashed: false,
-        }
-    }
-
-    fn has_crashed(&self) -> bool {
-        self.has_crashed
+impl fmt::Display for Tracks {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "tracks!")
     }
 }
 
